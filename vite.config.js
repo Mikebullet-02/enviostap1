a9067666fe4b7a6f63ba841e1995cfe01f224ec2
Mikebullet-02 <72115697+Mikebullet-02@users.noreply.github.com>
2023-04-20 09:22:16 -0600

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
-import {
-  defineConfig
-} from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-
-})
+  //Add proxy
+  server: {
+    proxy: {
+      "https://5vv8ikenvb.execute-api.us-east-1.amazonaws.com/default/correos":
+        {
+          target:
+            "https://5vv8ikenvb.execute-api.us-east-1.amazonaws.com/default/correos",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+    },
+  },
+});
